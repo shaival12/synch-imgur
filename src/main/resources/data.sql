da571af3b1957ab163170be52e6df2862168a0c6
sh <shaival.java@gmail.com>
2020-02-11 15:48:04 +0530

    123 :  changed configuration

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO 
 	TBL_EMPLOYEES (first_name, last_name, email) 
 VALUES
-  	('Lokesh', 'Gupta', 'howtodoinjava@gmail.com'),
+  	('Lokesh', 'Gupta', 'yuf@gmail.com'),
   	('John', 'Doe', 'xyz@email.com');
